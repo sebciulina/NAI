d7a41e4fd8f6402e6ec529a28e154ebe4ec897ac
sebciulina <sgransicki@gmail.com>
2022-11-08 19:17:17 +0100

    lab2 fixed

--- LAB2/main.cpp
+++ LAB2/main.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <random>
 #include <map>
+#include <algorithm>
 
 
 using domain_t = std::vector<double>;
@@ -10,44 +11,39 @@ std::random_device rd;
 std::mt19937 mt_generator(rd());
 
 domain_t
-hill_climbing(const std::function<double(domain_t)> &f, domain_t minimal_d, domain_t maximal_d, int max_iterations) {
-    domain_t current_p(minimal_d.size());
-    for (int i = 0; i < minimal_d.size(); i++) {
-        std::uniform_real_distribution<double> dist(minimal_d[i], maximal_d[i]);
-        current_p[i] = dist(mt_generator);
-    }
+hill_climbing(const std::function<double(domain_t)> &f, domain_t start_point, std::function<std::vector<domain_t>()> get_close_points, int max_iterations) {
+    domain_t best_p = start_point;
     for (int iteration = 0; iteration < max_iterations; iteration++) {
-        domain_t new_p(minimal_d.size());
-        for (int i = 0; i < minimal_d.size(); i++) {
-            std::uniform_real_distribution<double> dist(-1.0 / 128.0, 1.0 / 128.0);
-            double picked = current_p[i] + dist(mt_generator);
-            if (minimal_d[i] < picked && maximal_d[i] > picked) {
-                new_p[i] = picked;
-            }
-        }
-        if (f(current_p) > f(new_p)) {
-            current_p = new_p;
-        }
+        auto close_points = get_close_points();
+        auto best_neighbour = *std::min_element(close_points.begin(), close_points.end(), [f](auto a, auto b){return f(a) > f(b);});
+        if (f(best_neighbour) < f(best_p)) best_p = best_neighbour;
     }
-    return current_p;
+    return best_p;
 }
 
 int main(int argc, char **argv) {
     std::map<std::string, std::function<double(std::vector<double>)>> formatery_h;
     std::vector<std::string> argumenty(argv, argc + argv);
 
-    formatery_h["sphere"] = [](domain_t x) { return x[0] * x[0]; };
+    formatery_h["booth"] = [](domain_t x) {return pow((x[0]+ 2*x[1] - 7),2) + pow((2*x[0]+x[1]-5),2);};
     formatery_h["matyas"] = [](domain_t x) { return 0.26 * (x[0] * x[0] + x[1] * x[1]) - (0.48 * x[0] * x[1]); };
-    formatery_h["rosenbrock"] = [](domain_t x) { return 100 * pow((x[0] - (x[0] * x[0])), 2) + pow((1 - x[0]), 2); };
+    formatery_h["beale"] = [](domain_t x) {return pow((1.5-x[0]+x[0]*x[1]),2) + pow(2.25-x[0]+x[0]*x[1]*x[1],2) + pow(2.265-x[0]+x[0]*x[1]*x[1]*x[1],2);};
+
+    auto get_random_point = [=]() -> domain_t {
+        std::uniform_real_distribution<double>distr(std::stod(argumenty.at(2)), std::stod(argumenty.at(3)));
+        return {distr(mt_generator), distr(mt_generator)};
+    };
+    auto get_close_points_random = [=]() -> std::vector<domain_t> {
+        std::uniform_real_distribution<double>distr(std::stod(argumenty.at(2)),std::stod(argumenty.at(3)));
+        return {{distr(mt_generator), distr(mt_generator)}};
+    };
+
     try {
-        auto best = hill_climbing(formatery_h.at(argumenty.at(1)), {std::stod(argumenty.at(2))},
-                                  {std::stod(argumenty.at(3))}, 10000);
-        if (argumenty.at(1) == "matyas") {
-            std::cout << "best x = " << best[0] << std::endl;
-            std::cout << "best y = " << best[1] << std::endl;
-        } else {
-            std::cout << "best x = " << best[0] << std::endl;
-        }
+        auto best = hill_climbing(formatery_h.at(argumenty.at(1)), get_random_point(), get_close_points_random, 100000);
+        std::cout << "best x = " << best[0] << std::endl;
+        std::cout << "best y = " << best[1] << std::endl;
+
+
 
     }
     catch (std::out_of_range oof) {
